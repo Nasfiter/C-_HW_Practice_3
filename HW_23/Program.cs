959485963c3c513739ac859813bd4bcfc7a39beb
Alex Nejen <alex_nas@Mac-mini-Alex.local>
2022-05-23 08:47:05 -0400

    HW21 and Hw_23 is done.

---
+++ HW_23/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 23: Напишите программу, которая принимает на
+// вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 5 -> 1, 8, 27, 64, 125
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Ввседите целое число: ");
+int N = Convert.ToInt16(Console.ReadLine());
+
+int index = 1;
+//int number = index; 
+
+while (index < (N + 1))
+{
+    int number = index;
+    int result = number * number * number; 
+    Console.WriteLine($"Результат= {result}");
+    index++;
+}
